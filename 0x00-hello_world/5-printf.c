156c9ec0076117d3a49f9c957f7f449dba3898bd
abrhamamare <abrhamgetachew1234@gmail.com>
2022-03-11 07:10:15 -0800

    hello

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+/**
+ * main - print "with the proper outcome"
+ * Return: 0
+ */
+#include<stdio.h>
+int main(void)
+{printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
