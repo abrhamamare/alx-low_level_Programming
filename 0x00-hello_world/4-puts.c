ba0c87ddaf0dbf3828669d80bef2e84c172b57ca
abrhamamare <abrhamgetachew1234@gmail.com>
2022-03-11 06:07:35 -0800

    hit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(void)
  {
-	char[] s=""\Programming is like building a multilingual puzzle";
-puts(s);
+	char str[] =""\Programming is like building a multilingual puzzle";
+puts(str);
 return (0);
 }
