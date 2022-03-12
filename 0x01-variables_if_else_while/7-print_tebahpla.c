537db12ab160ac014a19ff14ef7a93625c9e6ff2
abrhamamare <abrhamgetachew1234@gmail.com>
2022-03-12 07:06:16 -0800

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+/**
+ * main - a function that prints the alphabet, in lowercase
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+        char i;
+
+        for (i = 'z'; i <= 'a'; i--)
+        {
+	putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
