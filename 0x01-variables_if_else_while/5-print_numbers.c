a4dd797071abd0e09630743463c6fda6421609e5
abrhamamare <abrhamgetachew1234@gmail.com>
2022-03-12 06:48:17 -0800

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+/**
+ * main - a function that prints the alphabet, in lowercase
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	int i;
+
+        for (i = 0; i <= 9; i++)
+                puts(i);
+	put('\n');
+	return (0);
+}
+
