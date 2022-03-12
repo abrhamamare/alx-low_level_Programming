dc852f61f3a03bfbe9acecb8fc65022098dd941a
abrhamamare <abrhamgetachew1234@gmail.com>
2022-03-12 06:11:05 -0800

    m

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,16 +5,18 @@
  *
  * Return: Always 0 (Succes)
  */
-int main()
+int main(void)
 {
-	char s[100]="abcdefghijklmnopqrstuvwxyz";
-	int i=0;
-	scanf("%[^\n]",s);
-	while (s[i]!='\0')
+	char s[100] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+
+	scanf("%[^\n]", s);
+	while (s[i] != '\0')
 	{
+
 	if (s[i] >= 'A' && s[i] <= 'Z') s[i] += 32;
 	i++;
-   }
+	}
 	puts(s);
-	return 0;
+	return (0);
 }
