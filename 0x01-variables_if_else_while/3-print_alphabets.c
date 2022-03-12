b1eccfdbd20602c9587ef60a5143932ded3c05a3
abrhamamare <abrhamgetachew1234@gmail.com>
2022-03-12 06:24:30 -0800

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+/**
+ * main - a function that prints the alphabet, in lowercase
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	char i, j;
+
+	for(i = 'a';i <= 'z';i++){
+	putchar(i);
+	}
+	for(j = 'A';j <= 'Z';A++){
+	putchar(j);
+	}
+	putchar('$');
+	return (0);
+}
+
