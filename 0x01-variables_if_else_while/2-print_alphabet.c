116f2b46ab050d316dcc87da643aa65ffb58ab80
abrhamamare <abrhamgetachew1234@gmail.com>
2022-03-12 05:58:41 -0800

    m

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,21 @@
 #include<stdio.h>
 #include<string.h>
-
 /**
  * print_alphabet - a function that prints the alphabet, in lowercase
  *
  * Return: Always 0 (Succes)
  */
-#include<stdio.h>
-#include<string.h>
 int main()
 {
 	char s[100];
-	puts("Enter a string: ");
+	int i=0;
+	printf("Enter a string: ");
 	scanf("%[^\n]",s);
-	puts(strlwr(s));
+	while (s[i]!='\0')
+	{
+	if (s[i] >= 'A' && s[i] <= 'Z') s[i] += 32;
+	i++;
+   }
+	puts(s);
 	return 0;
- }
+}
