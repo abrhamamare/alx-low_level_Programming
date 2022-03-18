d9b943d355435da5dfee864d41ff90d513e08a79
abrhamamare <abrhamgetachew1234@gmail.com>
2022-03-18 05:23:40 -0700

    hello

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * prnt_most_numbers - function print
+ * Descritpiton -print every number
+ * Return: o
+ */
+int print_most_numbers(void)
+{
+	int i;
+
+for(i = 0; i <= 9 ;i++){
+if(i == 2 || i == 9)
+	continue;
+_putchar(i);
+}
+_putchar('\n');
+}
