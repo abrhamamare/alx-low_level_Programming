85ed3e90b79f91c3e8520e0079b622edbb19afb8
abrhamamare <95219543+abrhamamare@users.noreply.github.com>
2022-03-25 23:44:40 -0700

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,22 @@
+#include "main.h"
+
 /**
- * main - print putchar
- * Return: 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-#include<unistd.h>
-int main(void){
-_putchar("_putchar");
+int main(void)
+{
+
+	_putchar(72);
+	_putchar(111);
+	_putchar(108);
+	_putchar(98);
+	_putchar(101);
+	_putchar(114);
+	_putchar(116);
+	_putchar(111);
+	_putchar(110);
+	_putchar ('\n');
 	return (0);
 }
-
