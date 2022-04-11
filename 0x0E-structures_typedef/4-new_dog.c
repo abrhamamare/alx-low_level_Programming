cd7fe2bd89491462c8acc9db92ca4d2a2ae8dd43
abrhamamare <95219543+abrhamamare@users.noreply.github.com>
2022-04-11 07:31:14 -0700

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,7 +8,7 @@
  * @owner: owner of dog
  * Return: Always 0.
  */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+dog_t *new_dog(char *name, float age, char *owner)
 {
 
 	if (d != NULL)
