89cdb56621f10461001b49f002ec167dc70fa421
abrhamamare <95219543+abrhamamare@users.noreply.github.com>
2022-04-22 07:21:29 -0700

    Create 0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - print all elements of `list_t` list
+ * @h: head of linked list
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t size;
+
+	size = 0;
+	while (h != NULL)
+	{
+		size++;
+		if (h->str == NULL)
+			printf("[%d] (nil)\n", 0);
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	return (size);
+}
