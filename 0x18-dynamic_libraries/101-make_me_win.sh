a8e53c9da6200fb45e2b095de1df9cc401aee166
abrhamam <abrhamgetachew1234@gmail.com>
2022-06-16 08:18:33 -0700

    hot fix2

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/abrhamamare/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
+wget -P /tmp https://github.com/abrhamamare/alx-low_level_Programming/blob/main/0x18-dynamic_libraries/libmask.so
 export LD_PRELOAD=/tmp/libmask.so
